10748c679cb3e78310b6fbe276a4e6d0b74dd89f
aksurya3456 <aksurya3456@gmail.com>
2017-10-04 15:15:39 +0530

    Request OTP change

--- requestOTP.php
+++ requestOTP.php
@@ -3,7 +3,7 @@
 header("Content-type: application/json; charset=UTF-8");
 
 $response = Array();
-$response["message"] = Array();
+// $response["message"] = Array();
 $request = json_decode(file_get_contents('php://input'),true);
 
 if($_SERVER['REQUEST_METHOD']=="POST")
@@ -11,19 +11,21 @@ if($_SERVER['REQUEST_METHOD']=="POST")
 	if($request['mobile'] != null)
 	{
 		$response["http_code"] = 200;
-		$response["message"]["message"] = "OTP sent successfully";
+		$response["message"] = Array();
+		$response["message"] = $request;
 	}
 	else
 	{
 		$response["http_code"] = 400;
-		$response["message"]["message"] = "Bad or ill-formed request";
+		$response["message"] = Array();
+		$response["message"] = $request;
 	}
 
 }
 else
 {
 	$response["http_code"] = 404;
-	$response["message"]["message"] = "Only POST request can be handled";
+	$response["message"] = "Only POST request can be handled";
 
 
 }
